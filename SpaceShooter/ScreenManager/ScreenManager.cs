186d188e626499964b93148cfe283c80db48097d
Nate <hodge.nate@gmail.com>
2013-03-16 16:50:40 -0400

    Initial Commit

---
+++ SpaceShooter/ScreenManager/ScreenManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceShooter.ScreenManager
+{
+    public class ScreenManager //TODO: : DrawableGameComponent
+    {
+        List<GameScreen> allScreens = new List<GameScreen>();
+        List<GameScreen> currentScreens = new List<GameScreen>();
+        InputState input = new InputState();
+        ContentManager content;
+    }
+}
